e0afe79a28a384e11d89a9ddc1298d18721060bb
saam <sami258000@gmail.com>
2022-04-08 16:02:24 +0430

    header problem fixed

--- src/component/Header.js
+++ src/component/Header.js
@@ -21,92 +21,94 @@ export default function Header() {
     window.location.reload();
   };
   return (
-    <div className={Style.container}>
-      <ul className={Style.list}>
-        <li>
-          <img className={Style.logo} src={LOGO} alt="logo" />
-        </li>
-        <li>
-          <h2>SHOPPY</h2>
-        </li>
-        <li>
-          <NavLink
-            to={"/"}
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                textDecoration: isActive ? "underline" : "",
-                color: isActive ? "white" : "black",
-              };
-            }}
-            className={Style.navlink}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={"/shop"}
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                textDecoration: isActive ? "underline" : "",
-                color: isActive ? "white" : "black",
-              };
-            }}
-          >
-            Shop
-          </NavLink>
-        </li>
-        {/* <li>
-          <NavLink
-            to={"/signup"}
-            style={({ isActive }) => {
-              return {
-                // display: "block",
-                // textDecoration: isActive ? "underline" : "",
-                color: isActive ? "white" : "black",
-              };
-            }}
-          >
-            {local.user ? <p>local.user</p> : <p>login</p>}
-          </NavLink>
-        </li> */}
-        <li>
-          <NavLink
-            to={"/signup"}
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                textDecoration: isActive ? "underline" : "",
-                color: isActive ? "white" : "black",
-              };
-            }}
-          >
-            {local.cond ? (
-              <p style={{ color: "red" }} onClick={logOutHandler}>
-                Logout
-              </p>
-            ) : (
-              ""
-            )}
-          </NavLink>
-        </li>
-        <li className={Style.liCart}>
-          <Link to={"/cart"}>
-            <img
-              alt="logo"
-              className={Style.cardImg}
-              src={
-                "https://img.icons8.com/fluency/48/000000/shopping-cart-loaded.png"
-              }
-            />
-            {state.totalCount >= 1 && (
-              <span className={Style.spanCount}>{state.totalCount}</span>
-            )}
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={Style.container}>
+        <ul className={Style.list}>
+          <li>
+            <img className={Style.logo} src={LOGO} alt="logo" />
+          </li>
+          <li>
+            <h2>SHOPPY</h2>
+          </li>
+          <li>
+            <NavLink
+              to={"/"}
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  textDecoration: isActive ? "underline" : "",
+                  color: isActive ? "white" : "black",
+                };
+              }}
+              className={Style.navlink}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/shop"}
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  textDecoration: isActive ? "underline" : "",
+                  color: isActive ? "white" : "black",
+                };
+              }}
+            >
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/signup"}
+              style={({ isActive }) => {
+                return {
+                  // display: "block",
+                  // textDecoration: isActive ? "underline" : "",
+                  color: isActive ? "white" : "black",
+                };
+              }}
+            >
+              {local.user ? <p>local.user</p> : <p>login</p>}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/signup"}
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  textDecoration: isActive ? "underline" : "",
+                  color: isActive ? "white" : "black",
+                };
+              }}
+            >
+              {local.cond ? (
+                <p style={{ color: "red" }} onClick={logOutHandler}>
+                  Logout
+                </p>
+              ) : (
+                ""
+              )}
+            </NavLink>
+          </li>
+          <li className={Style.liCart}>
+            <Link to={"/cart"}>
+              <img
+                alt="logo"
+                className={Style.cardImg}
+                src={
+                  "https://img.icons8.com/fluency/48/000000/shopping-cart-loaded.png"
+                }
+              />
+              {state.totalCount >= 1 && (
+                <span className={Style.spanCount}>{state.totalCount}</span>
+              )}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 }
